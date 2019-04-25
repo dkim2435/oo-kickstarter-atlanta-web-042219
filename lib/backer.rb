8a770828679cfa252c10b9f478f013a5ea8e23b6
evan <dkim2435@gmail.com>
2019-04-25 14:56:08 -0400

    finished

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+require 'pry'
+class Backer
+  attr_reader :name, :backed_projects
+ # @backed_projects = []
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project ### add project instance
+    project.backers << self
+
+  end
+end
